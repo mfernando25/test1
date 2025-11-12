57413fc11061d2178891d366e4ff1aceb0931d5b
Fernando <mangulos@continental.edu.pe>
2025-11-12 02:26:38 -0500

    aumentar data

--- prueba.py
+++ prueba.py
@@ -1,5 +1,5 @@
 def saludo(nombre: str) -> str:
-    return f"Hola, {nombre}! Bienvenido a Git, otra vez, desde git. Ahora desde la rama."
+    return f"Hola, {nombre}! Bienvenido a Git, otra vez, desde git. Ahora desde la rama.prueba dos"
 
 if __name__ == "__main__":
     nombre = input("¿Tu nombre? ")
