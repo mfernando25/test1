8684f96f519a8066e1e6f6302084c7e9b796ee5e
mike <reqlown@gmail.com>
2025-11-12 02:13:47 -0500

    agregar desde rama 2

--- prueba.py
+++ prueba.py
@@ -1,5 +1,5 @@
 def saludo(nombre: str) -> str:
-    return f"Hola, {nombre}! Bienvenido a Git, otra vez, desde git. Ahora desde la rama"
+    return f"Hola, {nombre}! Bienvenido a Git, otra vez, desde git. Ahora desde la rama. Unita más."
 
 if __name__ == "__main__":
     nombre = input("¿Tu nombre? ")
