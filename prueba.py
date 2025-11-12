ddfb300918ac24335ffef6ce1a2811ad299dcf19
mike <reqlown@gmail.com>
2025-11-12 07:41:29 -0500

    commit peque

--- prueba.py
+++ prueba.py
@@ -2,7 +2,7 @@
 import sys, matplotlib.pyplot as plt, networkx as nx
 from matplotlib.patches import FancyArrowPatch
 
-# === Definición del DFA ===
+# === Definición del DFA =====
 states = {"q0","q1","q2","q3","q4"}
 alphabet = {"a","b"}
 delta = {("q0","a"):"q1", 
