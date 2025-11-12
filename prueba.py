9c26c5041cf5a316da2a4d7c18ffaed94999c9fa
mike <reqlown@gmail.com>
2025-11-12 03:09:51 -0500

    Dejo de trackear venv y cachés

--- prueba.py
+++ prueba.py
@@ -6,7 +6,6 @@ from matplotlib.patches import FancyArrowPatch
 states = {"q0","q1","q2","q3","q4"}
 alphabet = {"a","b"}
 delta = {("q0","a"):"q1", 
-        ("q1","a"):"q4", 
         ("q1","b"):"q4", 
         ("q2","a"):"q0", 
         ("q2","b"):"q1", 
@@ -16,7 +15,7 @@ delta = {("q0","a"):"q1",
 
 q0, F = "q0", {"q4"}
 
-# === Simulación ===
+# === Simulación ====
 def run(s):
     q, steps = q0, [q0]
     for i,ch in enumerate(s):
